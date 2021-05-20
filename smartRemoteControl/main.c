d4e916d05068e0712eeca045958c23f3642c6437
michael scholten <michael-scholten@hotmail.nl>
2021-05-20 23:11:18 +0200

    Completed the smart remote control program

--- smartRemoteControl/main.c
+++ smartRemoteControl/main.c
@@ -6,7 +6,7 @@
 
 #include <avr/interrupt.h>
 #include "../library/proximitySensor.h"
-
+#include "../library/motor.h"
 
 void USART_init(){
     // clear UCSR1A
@@ -17,26 +17,74 @@ void USART_init(){
 
     // use 8 dat bits
     UCSR1C = (1 << UCSZ10) | (1 << UCSZ11);
+
+    UBRR1 = 103;
 }
+
+// stores the current speed
+volatile uint16_t speed = 0;
 
 ISR(USART1_RX_vect){
     // store the received char
     char c = UDR1;
-
-    // stores the current speed
-    static uint8_t speed = 0;
-
+    
     if(c >= '0' && c <= '9'){
-        speed = (c - '0') << 4;
+        speed = (c - '0') << 6;
+        OCR1A = speed;
+        OCR1B = speed;
     }else{
         c |= 32;
         if(c == 'w'){
+            motors_setDirectionBoth(forward);
+            OCR1A = speed;
+            OCR1B = speed;
         }else if(c == 's'){
+            motors_setDirectionBoth(backward);
+            OCR1A = speed;
+            OCR1B = speed;
         }else if(c == 'a'){
+            motors_rotateLeft();
+            OCR1A = speed;
+            OCR1B = speed;
         }else if(c == 'd'){
+            motors_rotateRight();
+            OCR1A = speed;
+            OCR1B = speed;
         }else if(c == 'q'){
-        }else if(c == 'e'){}
+            motors_setDirectionBoth(forward);
+            OCR1A = speed;
+            OCR1B >>= 1;
+        }else if(c == 'e'){
+            motors_setDirectionBoth(forward);
+            OCR1A >>= speed;
+            OCR1B = 1;
+        }
     }
 }
 
-int main(){}
+int main(){
+    uint8_t stopStart = 0;
+    motors_init();
+    USART_init();
+    sei();
+    uint16_t brightness[] = {4, 15, 32, 55, 85, 120};
+    SensorData frontSensor = {.side = frontProxSensor};
+    while(1){
+        proxSensor_read(&frontSensor, 1, brightness, sizeof(brightness) / sizeof(*brightness), 420);
+        if((frontSensor.withRightLeds | frontSensor.withLeftLeds) == sizeof(brightness) / sizeof(*brightness) && ((~PORTB & MOTOR_DIRECTION_LEFT) && (~PORTB & MOTOR_DIRECTION_RIGHT))){
+            stopStart++;
+            if(stopStart > 1){
+                stopStart = 0;
+                OCR1A = 0;
+                OCR1B = 0;
+            }
+        }else{
+            stopStart--;
+            if(stopStart < -1){
+                stopStart = 0;
+                OCR1A = speed;
+                OCR1B = speed;
+            }
+        }
+    }
+}
